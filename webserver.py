954f06e20cf1b192c41d3bce9733e5be4fb3f658
root <root@jarkomc8.com>
2015-11-09 19:28:16 +0700

    web server update

--- webserver.py
+++ webserver.py
@@ -1,10 +1,12 @@
 import sys
 import socket
+import io
+
 
 HOST, PORT = '', 8888
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+#sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 # Bind the socket to the port
 sock.bind((HOST,PORT))
 sock.listen(1)
@@ -21,15 +23,26 @@ while True:
     	# Receive the data in small chunks and retransmit it
     	# while True:
         data = connection.recv(1024)
-	print data   
-	    	
-	http_response = """\
-HTTP/1.1 200 OK
+	#print data   
+	filename = data.split()
+	#print filename[1]
+	file1 =filename[1]	
+	file2 =file1[1:]
+	print file2
+	#if file2 =="/" : 	
+	#	f = open('android.jpg','r+')
+	#	jpgdata = f.read()
+	#	f.close()
+	#	connection.send(outputdata[i])
+	#	http_response = "\HTTP/1.1 200 OK \n\n%s"%jpgdata
+	#else  : 
+	f= open(file2+".jpg",'r+')
+	jpgdata2 = f.read()
+	f.close()
+	http_response = "\HTTP/1.1 200 OK \n\n%s"%jpgdata2
 
-OK
-"""
 
-			# print >>sys.stderr, 'received "%s"' % data
+		# print >>sys.stderr, 'received "%s"' % data
             	# if data:
                 #	print >>sys.stderr, 'sending data back to the client'
                 #	connection.sendall(data)
